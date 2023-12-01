45906384c3579b4936d00dbb3c5a36821b76ec8a
Monish212 <117730963+Monish212@users.noreply.github.com>
2023-12-01 13:05:48 +0530

    Delete terraform directory

--- terraform/modules/ecr/variables.tf
+++
@@ -1,9 +0,0 @@
-# Variable for ECR Repository Name
-
-variable "ecr_repository_name" {
-  # Description of the variable
-  description = "Name for the ECR repository"
-  
-  # Type of the variable
-  type        = string
-}
